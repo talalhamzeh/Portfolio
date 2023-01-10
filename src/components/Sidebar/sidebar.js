480284f1bf83149e84dae5aad623536c347709f8
Talal <talalb.hamze@hotmail.com>
2023-01-10 13:24:11 +1100

    linkedin added

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -71,7 +71,7 @@ const Sidebar = () => {
       <ul>
         <li>
           <a
-            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            href="https://www.linkedin.com/in/talalhamzeh/"
             target="_blank"
             rel="noreferrer"
           >
@@ -84,7 +84,7 @@ const Sidebar = () => {
         </li>
         <li>
           <a
-            href="https://github.com/bobangajicsm"
+            href="https://github.com/talalhamzeh"
             target="_blank"
             rel="noreferrer"
           >
